143818aeb62b9261c7568ddb8505a89352d20843
grantgess <hhh.kqw146@gmail.com>
2025-03-13 22:16:19 +0300

    deleted

--- InventoryService/src/main/java/org/example/inventoryservice/exceptions/ProductNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package org.example.inventoryservice.exceptions;
-
-public class ProductNotCreatedException extends RuntimeException {
-  public ProductNotCreatedException(String message) {
-    super(message);
-  }
-}
